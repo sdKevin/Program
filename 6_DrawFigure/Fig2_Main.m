07659519408090035140379f78368e2960e5718e
Kaiwen <wkw123920909@126.com>
2020-04-14 17:21:08 +0800

    Fig2a_Left_Finish

--- 6_DrawFigure/Fig2_Main.m
+++ 6_DrawFigure/Fig2_Main.m
@@ -6,12 +6,14 @@ Path_ETrc_Year_ScenarioMIP_ssp126_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_
 Path_ETrc_Year_ScenarioMIP_ssp245_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_ETrc\ScenarioMIP_ssp245\ETrc_ssp245_ETrc_Year.mat';
 Path_ETrc_Year_ScenarioMIP_ssp370_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_ETrc\ScenarioMIP_ssp370\ETrc_ssp370_ETrc_Year.mat';
 Path_ETrc_Year_ScenarioMIP_ssp585_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_ETrc\ScenarioMIP_ssp585\ETrc_ssp585_ETrc_Year.mat';
+Path_ETrc_Year_Princeton_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_ETrc\Princeton\ETrc_Year_Princeton.mat';
 
 Path_Met_Year_Historical_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Met\Historical\Met_Var_Historical_Met_Year.mat';
 Path_Met_Year_ScenarioMIP_ssp126_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Met\ScenarioMIP_ssp126\Met_Var_ssp126_Met_Year.mat';
 Path_Met_Year_ScenarioMIP_ssp245_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Met\ScenarioMIP_ssp245\Met_Var_ssp245_Met_Year.mat';
 Path_Met_Year_ScenarioMIP_ssp370_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Met\ScenarioMIP_ssp370\Met_Var_ssp370_Met_Year.mat';
 Path_Met_Year_ScenarioMIP_ssp585_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Met\ScenarioMIP_ssp585\Met_Var_ssp585_Met_Year.mat';
+Path_Met_Year_Princeton_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Met\Princeton\Met_Year_Princeton.mat';
 
 Path_Attribution_Year_Historical_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Attribution\Historical\Attribution_Var_Historical_Attribution_Year.mat';
 Path_Attribution_Year_ScenarioMIP_ssp126_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Attribution\ScenarioMIP_ssp126\Attribution_Var_ssp126_Attribution_Year.mat';
@@ -19,19 +21,23 @@ Path_Attribution_Year_ScenarioMIP_ssp245_GCM = 'D:\CMIP6\VariableStorage\YearlyV
 Path_Attribution_Year_ScenarioMIP_ssp370_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Attribution\ScenarioMIP_ssp370\Attribution_Var_ssp370_Attribution_Year.mat';
 Path_Attribution_Year_ScenarioMIP_ssp585_GCM = 'D:\CMIP6\VariableStorage\YearlyVar\Var_Attribution\ScenarioMIP_ssp585\Attribution_Var_ssp585_Attribution_Year.mat';
 
+
 ETrc_Year = cat(2,load(Path_ETrc_Year_Historical_GCM),...
     load(Path_ETrc_Year_ScenarioMIP_ssp126_GCM),load(Path_ETrc_Year_ScenarioMIP_ssp245_GCM),...
-    load(Path_ETrc_Year_ScenarioMIP_ssp370_GCM),load(Path_ETrc_Year_ScenarioMIP_ssp585_GCM));
+    load(Path_ETrc_Year_ScenarioMIP_ssp370_GCM),load(Path_ETrc_Year_ScenarioMIP_ssp585_GCM),...
+    load(Path_ETrc_Year_Princeton_GCM));
 
 Met_Year = cat(2,load(Path_Met_Year_Historical_GCM),...
     load(Path_Met_Year_ScenarioMIP_ssp126_GCM),load(Path_Met_Year_ScenarioMIP_ssp245_GCM),...
-    load(Path_Met_Year_ScenarioMIP_ssp370_GCM),load(Path_Met_Year_ScenarioMIP_ssp585_GCM));
+    load(Path_Met_Year_ScenarioMIP_ssp370_GCM),load(Path_Met_Year_ScenarioMIP_ssp585_GCM),...
+    load(Path_Met_Year_Princeton_GCM));
 
 Attribution_Year = cat(2,load(Path_Attribution_Year_Historical_GCM),...
     load(Path_Attribution_Year_ScenarioMIP_ssp126_GCM),load(Path_Attribution_Year_ScenarioMIP_ssp245_GCM),...
     load(Path_Attribution_Year_ScenarioMIP_ssp370_GCM),load(Path_Attribution_Year_ScenarioMIP_ssp585_GCM));
 
 Fig2a_Left_Plotting(ETrc_Year , Met_Year)
+Fig2a_Right_Plotting(ETrc_Year , Met_Year)
 
 
 %%% Figure 2b
---
+++ 6_DrawFigure/Fig2a_Right_Plotting.m
@@ -0,0 +1,7 @@
+function Fig2a_Right_Plotting(inputArg1,inputArg2)
+%FIG2A_RIGHT_PLOTTING Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
