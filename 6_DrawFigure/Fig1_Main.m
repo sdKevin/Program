e64dba8adab93ca1a63f7f347535d799deb0ae47
Kaiwen <wkw123920909@126.com>
2020-09-21 16:40:51 +0800

    Fig1.b 3*3Matrix

--- 6_DrawFigure/Fig1_Main.m
+++ 6_DrawFigure/Fig1_Main.m
@@ -12,11 +12,11 @@ CMIP_DryWetRegion = cat(2,load(Path_DryWetRegion_Historical_GCM),...
     load(Path_DryWetRegion_ScenarioMIP_ssp126_GCM),load(Path_DryWetRegion_ScenarioMIP_ssp245_GCM),...
     load(Path_DryWetRegion_ScenarioMIP_ssp370_GCM),load(Path_DryWetRegion_ScenarioMIP_ssp585_GCM));
 
-% Plotting Global Cold Lands under different scenarios
+% Plotting Global Cold Regions under different scenarios
 Fig1a_ColdLands_Left_Plotting( CMIP_DryWetRegion , Path_DryWetRegion_Princeton );
 Fig1a_ColdLands_Right_Plotting( CMIP_DryWetRegion );
 
-% Plotting Global Cold Lands, Dry Lands, and Humid Lands
+% Plotting Global Cold Regions, Dry Lands, and Humid Lands
 Fig1a_GlobalLands_Left_Plotting( cat(2,CMIP_DryWetRegion(1),CMIP_DryWetRegion(5)) , Path_DryWetRegion_Princeton );
 Fig1a_GlobalLands_Right_Plotting( CMIP_DryWetRegion );
 
